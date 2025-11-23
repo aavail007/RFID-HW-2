6d23413658a30940c5239c71155b722935aeaec4
limeng-laptop <aavail007@gmail.com>
2025-11-23 14:22:52 +0800

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace WindowsFormsApplication6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnReadData = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbSector = new System.Windows.Forms.ComboBox();
+            this.cbBlock = new System.Windows.Forms.ComboBox();
+            this.cbKeyType = new System.Windows.Forms.ComboBox();
+            this.loadKey = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(111, 18);
+            this.Label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(63, 18);
+            this.Label2.TabIndex = 12;
+            this.Label2.Text = "Block : ";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(13, 18);
+            this.Label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(67, 18);
+            this.Label1.TabIndex = 13;
+            this.Label1.Text = "Sector : ";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(171, 159);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(128, 34);
+            this.btnClose.TabIndex = 11;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(16, 111);
+            this.txtResult.Margin = new System.Windows.Forms.Padding(4);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(333, 29);
+            this.txtResult.TabIndex = 10;
+            // 
+            // btnReadData
+            // 
+            this.btnReadData.Enabled = false;
+            this.btnReadData.Location = new System.Drawing.Point(357, 111);
+            this.btnReadData.Margin = new System.Windows.Forms.Padding(4);
+            this.btnReadData.Name = "btnReadData";
+            this.btnReadData.Size = new System.Drawing.Size(112, 34);
+            this.btnReadData.TabIndex = 8;
+            this.btnReadData.Text = "Read Data";
+            this.btnReadData.UseVisualStyleBackColor = true;
+            this.btnReadData.Click += new System.EventHandler(this.btnReadData_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(215, 18);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 18);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "KeyAB : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(318, 18);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 18);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Load Key :";
+            // 
+            // cbSector
+            // 
+            this.cbSector.FormattingEnabled = true;
+            this.cbSector.Location = new System.Drawing.Point(16, 51);
+            this.cbSector.Name = "cbSector";
+            this.cbSector.Size = new System.Drawing.Size(81, 26);
+            this.cbSector.TabIndex = 16;
+            // 
+            // cbBlock
+            // 
+            this.cbBlock.FormattingEnabled = true;
+            this.cbBlock.Location = new System.Drawing.Point(114, 51);
+            this.cbBlock.Name = "cbBlock";
+            this.cbBlock.Size = new System.Drawing.Size(81, 26);
+            this.cbBlock.TabIndex = 17;
+            // 
+            // cbKeyType
+            // 
+            this.cbKeyType.FormattingEnabled = true;
+            this.cbKeyType.Location = new System.Drawing.Point(218, 51);
+            this.cbKeyType.Name = "cbKeyType";
+            this.cbKeyType.Size = new System.Drawing.Size(81, 26);
+            this.cbKeyType.TabIndex = 18;
+            // 
+            // loadKey
+            // 
+            this.loadKey.Location = new System.Drawing.Point(321, 48);
+            this.loadKey.Name = "loadKey";
+            this.loadKey.Size = new System.Drawing.Size(148, 29);
+            this.loadKey.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 206);
+            this.Controls.Add(this.loadKey);
+            this.Controls.Add(this.cbKeyType);
+            this.Controls.Add(this.cbBlock);
+            this.Controls.Add(this.cbSector);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnReadData);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.Button btnClose;
+        internal System.Windows.Forms.TextBox txtResult;
+        internal System.Windows.Forms.Button btnReadData;
+        internal System.Windows.Forms.Label label3;
+        internal System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbSector;
+        private System.Windows.Forms.ComboBox cbBlock;
+        private System.Windows.Forms.ComboBox cbKeyType;
+        private System.Windows.Forms.TextBox loadKey;
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,21 @@
+﻿# RFID-HW-1 專案說明
+
+## EasyPOD.dll 使用說明
+
+1. **EasyPOD.dll 不放入 Git**
+   - 請勿將 `EasyPOD.dll` 加入 Git 版本控制（已在 `.gitignore` 設定排除）。
+
+2. **手動複製 EasyPOD.dll**
+   - 請手動將 `EasyPOD.dll` 複製到專案根目錄下的 `libs` 資料夾。
+   - 路徑範例：  
+     ```
+     MyProject/
+      libs/
+      WindowsFormsApplication6.csproj
+     ```
+
+3. **Build 時自動複製**
+   - 專案在建置（Build）時，會自動將 `libs\EasyPOD.dll` 複製到輸出資料夾（`bin\Debug` 或 `bin\Release`），並放在輸出資料夾的根目錄。
+   - 不需手動複製到 `bin` 目錄，建置流程會自動處理。
+
+---
